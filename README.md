21fcb5483b400c64c00d8ac99ccaa0d73134a10c
Edmund Jorgensen <tomheon@gmail.com>
2013-05-04 15:30:23 -0400

    Add first cut at ldap json backend.
    This has been manually tested, but has no automated tests.

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-strings-hiera-ldap
-==================
+# strings-hiera-ldap
 
 Bitlancer Strings LDAP backend for hiera
+
+## In a Nutshell
+
+Provides a new hiera backend, "ldapjson", which can be configured to
+pull json from ldap and then query it in the same manner as the normal
+json backend.
+
+## Configuration
+
+Example configuration:
+
+```
+---
+:backends:
+  - ldapjson
+
+:logger: console
+
+:hierarchy:
+  - common
+
+:ldapjson:
+   :ldap_host: 'localhost'
+   :ldap_port: 389
+   :ldap_base: 'dc=testing,dc=com'
+   :ldap_bind_dn: 'cn=Manager,dc=testing,dc=com'
+   :ldap_bind_password: 'password'
+   :ldap_filter: '(objectclass=domain)'
+   :ldap_attr: 'description'
+```
+
+The ldap_filter value can (and should) have interpolated scope values
+in it (e.g. ```%{env}``` or ```%{location}```), as these values will
+be filled in from the scope of the query.  For convenience, the
+```%{key}``` value is also interpolated in the ldap_filter.
+
+All ldapjson fields are required.
+
+## Query Handling
+
+Query handling is pretty much the same as the normal json backend,
+with one added wrinkle: whereas the json backend will merge multiple
+json files and json arrays on an array or hash query, the ldap json
+backend will merge multiple entries returned from an ldap search, json
+arrays, **and** multiple values for the target attr in each of those
+entries.  In other words, if you had, using the above (kind of
+nonsense) configuration, two entries in ldap:
+
+```
+dn: cn=ewj,dc=testing,dc=com
+objectClass: domain
+objectClass: extensibleObject
+dc: hi
+description: {"testkey": "test value one"}
+
+dn: cn=ewj2,dc=testing,dc=com
+objectClass: domain
+objectClass: extensibleObject
+dc: hi2
+description: {"testkey": "test value two"}
+description: {"testkey": ["test value three", "test value four"]}
+```
+
+and you ran
+
+```hiera -a testkey```
+
+You'd get back all four of "test value one", "test value two", "test
+value three", and "test value four".
---
+++ hiera/backend/ldapjson_backend.rb
@@ -0,0 +1,98 @@
+class Hiera
+  module Backend
+    class Ldapjson_backend
+
+      def initialize(cache=nil)
+        require 'json'
+        require 'ldap'
+
+        Hiera.debug("Hiera LdapJson backend starting")
+      end
+
+      def lookup(key, scope, order_override, resolution_type)
+        answer = nil
+
+        Hiera.debug("Looking up #{key} in LdapJson backend")
+
+        ldap_host = Config[:ldapjson][:ldap_host]
+        Hiera.debug("Found config: ldap_host=#{ldap_host}")
+        ldap_port = Config[:ldapjson][:ldap_port]
+        Hiera.debug("Found config: ldap_host=#{ldap_port}")
+        ldap_base = Config[:ldapjson][:ldap_base]
+        Hiera.debug("Found config: ldap_base=#{ldap_base}")
+        ldap_bind_dn = Config[:ldapjson][:ldap_bind_dn]
+        Hiera.debug("Found config: ldap_bind_dn=#{ldap_bind_dn}")
+        ldap_bind_password = Config[:ldapjson][:ldap_bind_password]
+        Hiera.debug("Found config: ldap_bind_password=******")
+        ldap_filter = Config[:ldapjson][:ldap_filter]
+        Hiera.debug("Found config: ldap_filter=#{ldap_filter}")
+        ldap_attr = Config[:ldapjson][:ldap_attr]
+        Hiera.debug("Found config: ldap_attr=#{ldap_attr}")
+
+        ldap_filter = Backend.parse_string(ldap_filter, scope, {"key" => key})
+        Hiera.debug("After substitution, ldap_filter=#{ldap_filter}")
+
+        conn = LDAP::Conn.new(ldap_host, ldap_port)
+        conn.bind(ldap_bind_dn, ldap_bind_password) do |bound|
+          Hiera.debug("Ldap bind successful")
+          bound.search(ldap_base, LDAP::LDAP_SCOPE_SUBTREE, filter=ldap_filter,
+                       attrs=[ldap_attr]) do |entry|
+            Hiera.debug("Processing search entry...")
+            attrs = entry[ldap_attr]
+            if attrs.nil? or attrs.empty?
+              Hiera.debug("Couldn't find attr, skipping.")
+              next
+            end
+            for json_data in attrs
+              if json_data.nil? or json_data.empty?
+                Hiera.debug("No json data in attr, skipping.")
+                next
+              end
+              data = JSON.parse(json_data)
+              if data.empty? or not data.include?(key)
+                Hiera.debug("Json data didn't contain key, skipping.")
+                next
+              end
+              # for array resolution we just append to the array
+              # whatever we find, we then goes onto the next attr
+              # value / search results and keep adding to the array
+              #
+              # for priority searches we break after the first found
+              # data item
+              new_answer = Backend.parse_answer(data[key], scope)
+              case resolution_type
+              when :array
+                unless new_answer.kind_of? Array or new_answer.kind_of? String
+                  ex = "Expected Array and got #{new_answer.class}"
+                  raise Exception, ex
+                end
+                answer ||= []
+                Hiera.debug("Merging new answer to array.")
+                answer << new_answer
+              when :hash
+                unless new_answer.kind_of? Hash
+                  ex = "Expected Hash and got #{new_answer.class}"
+                  raise Exception, ex
+                end
+                answer ||= {}
+                Hiera.debug("Merging new answer to hash.")
+                answer = Backend.merge_answer(new_answer,answer)
+              else
+                # priority search...break after first item.
+                Hiera.debug("Found answer, breaking.")
+                answer = new_answer
+                break
+              end
+            end # for json_data in attrs
+
+          end # bound.search
+
+        end # conn.bind
+
+        return answer
+
+      end # def lookup
+
+    end # class Ldapjson_backend
+  end # module Backend
+end # class Hiera
